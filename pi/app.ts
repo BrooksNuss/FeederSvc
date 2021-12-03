cd3723116c6511da3ec5fc88693861b72c7c055e
Brooks Nuss <bnussjr@gmail.com>
2021-12-03 00:06:41 -0500

    Added queue access and build

--- pi/app.ts
+++ pi/app.ts
@@ -2,26 +2,53 @@ import {Consumer} from 'sqs-consumer';
 import AWS from 'aws-sdk';
 import { Agent } from 'https';
 
-const app = Consumer.create({
-    queueUrl: '',
-    handleMessage: async (message) => {
-        //TODO: add some handlers here and there
-    },
-    sqs: new AWS.SQS({
-        httpOptions: {
-            agent: new Agent({
-                keepAlive: true
-            })
-        }
-    })
+const credentials = new AWS.SharedIniFileCredentials({profile: 'pi-sqs-consumer'});
+const region = 'us-east-1';
+const queueName = 'FeederQueue';
+const awslocal = AWS.config;
+AWS.config.region = region;
+AWS.config.credentials = credentials;
+
+const sqs = new AWS.SQS({
+	httpOptions: {
+		agent: new Agent({
+			keepAlive: true
+		})
+	},
+	region: region,
 });
 
-app.on('error', (err) => {
-    console.error(err.message);
-});
+const getQueueUrl = async () => {
+	const queueList = (await sqs.listQueues().promise()).QueueUrls;
+	const queueUrl = queueList?.find(queue => queue.includes(queueName));
+	
+	if (!queueUrl) {
+		console.error("Specified queue does not exist");
+		return;
+	}
 
-app.on('processing_error', (err) => {
-    console.error(err.message);
-})
+	return queueUrl;
+}
 
-app.start();
+(async () => {
+	const queueUrl = await getQueueUrl();
+
+	const app = Consumer.create({
+		queueUrl,
+		handleMessage: async (message) => {
+			//TODO: add some handlers here and there
+			console.log(message);
+		},
+		sqs
+	});
+
+	app.on('error', (err) => {
+		console.error(err.message);
+	});
+
+	app.on('processing_error', (err) => {
+		console.error(err.message);
+	});
+
+	app.start();
+})();
